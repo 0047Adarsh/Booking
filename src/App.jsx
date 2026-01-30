c0750b3930e53d89fb3210fd8f82792ec172612a
Adarsh A Devadiga <adarsh9742007@gmail.com>
2026-01-31 00:22:26 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+  count;
+  setCount;
+  return (
+    <>
+      <h1>Hello</h1>
+    </>
+  )
+}
+
+export default App
